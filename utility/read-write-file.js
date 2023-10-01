cf788d367cfd6a5eb1eff15b22eae5a599378538
ignatiusomeje <mrexcel153@gmail.com>
2023-10-01 11:34:48 +0100

    fix: error can't set header after sending.

--- utility/read-write-file.js
+++ utility/read-write-file.js
@@ -5,28 +5,9 @@ const {readFile,writeFile} = require("fs").promises;
 const readFileFunc = async () => {
   try {
     const info = await readFile('./db/videos.json', 'utf8')
-    // if (info.errno){
-    //   return info
-    // }
     const parsedVideo = JSON.parse(info);
     return parsedVideo
-    
-  //  return await fs.readFile("./db/videos.json", "utf8", async (err, data) => {
-  //     if (err) {
-  //       console.log(err, 'error 1')
-  //       cb(err)
-  //       return err
-  //     }
-  //     const parsedVideo = JSON.parse(data);
-  //     if (parsedVideo.length < 1) {
-  //       cb(null, [])
-  //       return [];
-  //     }
-  //     cb(null, parsedVideo)
-  //     return parsedVideo;
-  //   });
   } catch (error) {
-    // console.log(error, 'error 2')
     return error
   }
 };
